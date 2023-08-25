42091acc6351dfc114be243efb812fa3bbc3bb90
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-08-25 15:18:22 +0530

    Update evenno-forloop.sh

--- evenno-forloop.sh
+++ evenno-forloop.sh
@@ -3,3 +3,15 @@ for ((i=0; i<=20; i=i+2))
 do
 echo "evenno: $i"
 done
+
+
+
+echo "Enter a number : "
+read n
+rem=$(( $n % 2 ))
+if [ $rem -eq 0 ]
+then
+echo "$n is even number"
+else
+echo "$n is odd number"
+fi
